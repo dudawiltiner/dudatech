9bf5ac605ceae003ce6b76267af29502ce50fc43
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-11-25 19:29:57 -0300

    Adicionando todo  design

--- components/footer.js
+++ components/footer.js
@@ -3,11 +3,11 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="flex justify-end mt-10 mr-4 space-x-6">
-      <a className="transition duration-300 ease-in-out transform border-t-4 rounded-full shadow-sm cursor-pointer hover:-translate-y-1 hover:scale-105"><img src="./linkedin.svg"/></a>
-      <a className="transition duration-300 ease-in-out transform border-t-4 rounded-full shadow-sm cursor-pointer hover:-translate-y-1 hover:scale-105"><img src="./insta.svg"/></a>
-      <a className="transition duration-300 ease-in-out transform border-t-4 rounded-full shadow-sm cursor-pointer hover:-translate-y-1 hover:scale-105"><img src="./port.svg"/></a>
-      <a className="transition duration-300 ease-in-out transform border-t-4 rounded-full shadow-sm cursor-pointer hover:-translate-y-1 hover:scale-105"><img src="./youtube.svg"/></a>
-      <a className="transition duration-300 ease-in-out transform border-t-4 rounded-full shadow-sm cursor-pointer hover:-translate-y-1 hover:scale-105"><img src="./git.svg"/></a>
+      <a href="https://www.linkedin.com/in/eduardawiltiner/" target="_blank" className="transition duration-300 ease-in-out transform border-t-4 rounded-full shadow-sm cursor-pointer hover:-translate-y-1 hover:scale-105"><img src="./linkedin.svg"/></a>
+      <a href="" target="_blank" className="transition duration-300 ease-in-out transform border-t-4 rounded-full shadow-sm cursor-pointer hover:-translate-y-1 hover:scale-105"><img src="./insta.svg"/></a>
+      <a href="https://dudaportfolio.vercel.app/" target="_blank" className="transition duration-300 ease-in-out transform border-t-4 rounded-full shadow-sm cursor-pointer hover:-translate-y-1 hover:scale-105"><img src="./port.svg"/></a>
+      <a href="" target="_blank" className="transition duration-300 ease-in-out transform border-t-4 rounded-full shadow-sm cursor-pointer hover:-translate-y-1 hover:scale-105"><img src="./youtube.svg"/></a>
+      <a href="https://github.com/dudawiltiner" target="_blank" className="transition duration-300 ease-in-out transform border-t-4 rounded-full shadow-sm cursor-pointer hover:-translate-y-1 hover:scale-105"><img src="./git.svg"/></a>
     </div>
   )
 }
--- components/main.js
+++ components/main.js
@@ -17,6 +17,15 @@ export default function Main() {
       <img className="absolute" src="./topFigure.svg"/>
       <img className="sticky z-50 w-3/5 mt-20 animate-bounce-slow" src="./logo.svg"/>
       <div className="flex flex-col items-center mx-10">
+      { !show &&
+        <div className="flex justify-end w-full">
+          <button onClick={() => setShow(true)}  type="button" className="absolute p-2 mb-4 rounded-md shadow-md -translate-y-14 bg-yellow-color">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M7.707 3.293a1 1 0 010 1.414L5.414 7H11a7 7 0 017 7v2a1 1 0 11-2 0v-2a5 5 0 00-5-5H5.414l2.293 2.293a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+            </svg>
+          </button>
+        </div>
+      }
       { show ? 
       
       links.map((item, idx) => (
@@ -29,26 +38,16 @@ export default function Main() {
         >
         {item.name}
         </button>
-      )) 
+      ))
       
       :
 
-      <div>
-        <div className="flex justify-end">
-          <button onClick={() => setShow(true)}  type="button" className="absolute p-2 mb-4 rounded-md shadow-md -translate-y-14 bg-yellow-color">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M7.707 3.293a1 1 0 010 1.414L5.414 7H11a7 7 0 017 7v2a1 1 0 11-2 0v-2a5 5 0 00-5-5H5.414l2.293 2.293a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
-            </svg>
-          </button>
-        </div>
-    
-        {content.map((item) => (
-          <button data-aos="zoom-in" href={item.url} className="w-full p-2 mt-4 mb-4 mr-4 font-medium text-center transition duration-300 ease-in-out transform border-t-4 rounded-md shadow-md cursor-pointer hover:-translate-y-1 hover:scale-105 border-black-color bg-yellow-color">
-            <a href={item.url}>{item.name}</a>
-          </button>))}
+      content.map((item) => (
+        <button data-aos="zoom-in" href={item.url} className="w-full p-2 mt-4 mb-4 mr-4 font-medium text-center transition duration-300 ease-in-out transform border-t-4 rounded-md shadow-md cursor-pointer hover:-translate-y-1 hover:scale-105 border-black-color bg-yellow-color">
+          <a href={item.url}>{item.name}</a>
+        </button>))
        
-      </div>
-       }
+      }
       </div>
       <Footer />
       <img src="./bottomFigure.svg"/>
