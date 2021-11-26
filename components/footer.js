f5be6cb411e1a127a128a8495a1fc2bca48b4842
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-11-26 17:32:08 -0300

    Links atualizados

--- components/footer.js
+++ components/footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
       <a href="https://www.linkedin.com/in/eduardawiltiner/" target="_blank" className="transition duration-300 ease-in-out transform border-t-4 rounded-full shadow-sm cursor-pointer hover:-translate-y-1 hover:scale-105"><img src="./linkedin.svg"/></a>
       <a href="" target="_blank" className="transition duration-300 ease-in-out transform border-t-4 rounded-full shadow-sm cursor-pointer hover:-translate-y-1 hover:scale-105"><img src="./insta.svg"/></a>
       <a href="https://dudaportfolio.vercel.app/" target="_blank" className="transition duration-300 ease-in-out transform border-t-4 rounded-full shadow-sm cursor-pointer hover:-translate-y-1 hover:scale-105"><img src="./port.svg"/></a>
-      <a href="" target="_blank" className="transition duration-300 ease-in-out transform border-t-4 rounded-full shadow-sm cursor-pointer hover:-translate-y-1 hover:scale-105"><img src="./youtube.svg"/></a>
+      <a href="https://www.youtube.com/channel/UCsyYo5sKxUlVbZWHhc0p3rw" target="_blank" className="transition duration-300 ease-in-out transform border-t-4 rounded-full shadow-sm cursor-pointer hover:-translate-y-1 hover:scale-105"><img src="./youtube.svg"/></a>
       <a href="https://github.com/dudawiltiner" target="_blank" className="transition duration-300 ease-in-out transform border-t-4 rounded-full shadow-sm cursor-pointer hover:-translate-y-1 hover:scale-105"><img src="./git.svg"/></a>
     </div>
   )
--- data/links.js
+++ data/links.js
@@ -3,20 +3,20 @@ const links = [
     name: 'ACESSE OS REPOSITÓRIOS',
     content: [
       {
-        name: "DESAFIO TRYBE EBYTR",
-        url: "skjjs"
+        name: "Java",
+        url: "https://github.com/dudawiltiner/java"
       },
       {
-        name: "DESAFIO TRYBE EBYTR",
-        url: "jhsjhs"
+        name: "Desafio Ebytr",
+        url: "https://github.com/dudawiltiner/desafio-tecnico-ebytr"
       },
       {
-        name: "DESAFIO TRYBE EBYTR",
-        url: "skjjs"
+        name: "Cronômetro Temático",
+        url: "https://github.com/dudawiltiner/cronometrotematico"
       },
       {
-        name: "DESAFIO TRYBE EBYTR",
-        url: "jhsjhs"
+        name: "LEDTR - Leitura de Dados em Tempo Real",
+        url: "https://github.com/dudawiltiner/ledtr"
       }
     ]
   },
@@ -24,12 +24,57 @@ const links = [
     name: 'LEIA OS MEUS ARTIGOS',
     content: [
       {
-        name: "DESAFIO TRYBE EBYTR",
-        url: "skjjs"
+        name: '"Desenvolvedor(a) Freelancer - Como conseguir o seu primeiro cliente e se sair bem?”',
+        url: "https://www.linkedin.com/pulse/desenvolvedora-freelancer-como-conseguir-o-seu-e-se-reis-santana/"
       },
       {
-        name: "DESAFIO TRYBE EBYTR",
-        url: "jhsjhs"
+        name: '“Porque estudar Desenvolvimento Web é uma ótima escolha quem gosta de Internet das Coisas”',
+        url: "https://www.linkedin.com/pulse/porque-estudar-desenvolvimento-web-%C3%A9-uma-%C3%B3tima-para-de-reis-santana/"
+      }
+      ,
+      {
+        name: '“8 dicas para quem quer passar em processos seletivos de alta concorrência”',
+        url: "https://www.linkedin.com/pulse/8-dicas-para-quem-quer-passar-em-processos-seletivos-de-reis-santana/"
+      }
+      ,
+      {
+        name: '“Você nunca mais vai esquecer como a Internet funciona”',
+        url: "https://www.linkedin.com/pulse/voc%C3%AA-nunca-mais-vai-esquecer-como-internet-funciona-reis-santana/"
+      }
+      ,
+      {
+        name: '“Por que os Analistas de Dados são importantes para o avanço da Internet?”',
+        url: "https://www.linkedin.com/pulse/import%C3%A2ncia-dos-analistas-de-dados-avan%C3%A7o-da-internet-reis-santana/"
+      }
+      ,
+      {
+        name: '“4 Livros para perder o medo de falar em público”',
+        url: "https://www.linkedin.com/pulse/4-livros-para-perder-o-medo-de-falar-em-p%25C3%25BAblico-reis-santana/"
+      }
+      ,
+      {
+        name: '“Porque estudar Desenvolvimento Web é uma ótima escolha quem gosta de Internet das Coisas”',
+        url: "https://www.linkedin.com/pulse/porque-estudar-desenvolvimento-web-%C3%A9-uma-%C3%B3tima-para-de-reis-santana/"
+      }
+      ,
+      {
+        name: '“Como ler mais livros em pouco tempo e, ainda por cima, aumentar a visibilidade do seu perfil no LinkedIn?”',
+        url: "https://www.linkedin.com/pulse/como-ler-mais-livros-em-pouco-tempo-com-um-alto-e-por-reis-santana/"
+      }
+      ,
+      {
+        name: '“Afinal, o que realmente é essa Inteligência Silenciosa?”',
+        url: "https://www.linkedin.com/pulse/afinal-o-que-realmente-%C3%A9-essa-intelig%C3%AAncia-silenciosa-reis-santana/"
+      }
+      ,
+      {
+        name: '“IoT - Revolucionando o Universo dos Negócios”',
+        url: "https://www.linkedin.com/pulse/como-usar-iot-para-alavancar-o-seu-neg%C3%B3cio-bruce-reis-santana/"
+      }
+      ,
+      {
+        name: '“Steve Jobs - Impactando o mundo”',
+        url: "https://www.linkedin.com/pulse/steve-jobs-impactando-o-mund-eduarda-wiltiner-reis-santana/"
       }
     ]
   },
@@ -37,12 +82,48 @@ const links = [
     name: 'RECOMENDO LER',
     content: [
       {
-        name: "DESAFIO TRYBE EBYTR",
-        url: "skjjs"
+        name: '"Ted Talks. O Guia Oficial do Ted Para Falar em Público"',
+        url: "https://www.amazon.com.br/gp/product/858057935X/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=858057935X&linkCode=as2&tag=dudawiltiner-20&linkId=bededb4fd7029fd43d512809318eb58e"
       },
       {
-        name: "DESAFIO TRYBE EBYTR",
-        url: "jhsjhs"
+        name: '"Storytelling: Aprenda a contar histórias com Steve Jobs, Papa Francisco, Churchill e outras lendas da Liderança"',
+        url: "https://www.amazon.com.br/gp/product/B085PXWCR6/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B085PXWCR6&linkCode=as2&tag=dudawiltiner-20&linkId=aca3503868cf662cf0b98464aa44b42b"
+      },
+      {
+        name: '"A Sutil Arte de Ligar o F*da-Se: Uma estratégia inusitada para uma vida melhor"',
+        url: "https://www.amazon.com.br/gp/product/855100249X/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=855100249X&linkCode=as2&tag=dudawiltiner-20&linkId=1f1da546034a733ec94425fd0b7c44c3"
+      },
+      {
+        name: '"Ted Talks. O Guia Oficial do Ted Para Falar em Público"',
+        url: "https://www.amazon.com.br/gp/product/858057935X/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=858057935X&linkCode=as2&tag=dudawiltiner-20&linkId=bededb4fd7029fd43d512809318eb58e"
+      },
+      {
+        name: '"IoT: Como Usar a "Internet das Coisas" Para Alavancar Seus Negócios"',
+        url: "https://www.amazon.com.br/gp/product/8551303562/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=8551303562&linkCode=as2&tag=dudawiltiner-20&linkId=d9ad962ceb13f0c9318a40b12f76f722"
+      },
+      {
+        name: '"The Silent Intelligence - The Internet of Things (English Edition) "',
+        url: "https://www.amazon.com.br/gp/product/B00FD5MG4A/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00FD5MG4A&linkCode=as2&tag=dudawiltiner-20&linkId=f057405bfd365a3e2e4d37a7c5d1c778"
+      },
+      {
+        name: '"IOT - Internet das coisas: Fundamentos e aplicações em Arduino e NodeMCU"',
+        url: "https://www.amazon.com.br/gp/product/8536526076/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=8536526076&linkCode=as2&tag=dudawiltiner-20&linkId=537217681b75bdf791dbe86fc0a6dc61"
+      },
+      {
+        name: '"Ted Talks. O Guia Oficial do Ted Para Falar em Público"',
+        url: "https://www.amazon.com.br/gp/product/858057935X/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=858057935X&linkCode=as2&tag=dudawiltiner-20&linkId=bededb4fd7029fd43d512809318eb58e"
+      },
+      {
+        name: '"Nômade Digital: um guia para você viver e trabalhar como e onde quiser"',
+        url: "https://www.amazon.com.br/gp/product/8551306189/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=8551306189&linkCode=as2&tag=dudawiltiner-20&linkId=bf50230996878020a8fbf2d4a01dae4a"
+      },
+      {
+        name: '"Steve Jobs"',
+        url: "https://www.amazon.com.br/Steve-Jobs-Walter-Isaacson/dp/8535919716/ref=asc_df_8535919716/?tag=googleshopp00-20&linkCode=df0&hvadid=379787775008&hvpos=&hvnetw=g&hvrand=8488253338477498113&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1001521&hvtargid=pla-681387859343&psc=1"
+      },
+      {
+        name: '"Aprendendo Inteligência: Manual de instruções do cérebro para estudantes em geral: 1"',
+        url: "https://www.amazon.com.br/gp/product/8576572052/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=8576572052&linkCode=as2&tag=dudawiltiner-20&linkId=75e114bd74e42307d21b1b01198f708f"
       }
     ]
   },
@@ -50,12 +131,20 @@ const links = [
     name: 'PROJETOS ESPECIAIS',
     content: [
       {
-        name: "DESAFIO TRYBE EBYTR",
-        url: "skjjs"
+        name: "Desafio Ebytr",
+        url: "https://desafioebytr.vercel.app/"
       },
       {
-        name: "DESAFIO TRYBE EBYTR",
-        url: "jhsjhs"
+        name: "Cronômetro Temático",
+        url: "https://dudawiltiner.github.io/cronometrotematico/"
+      },
+      {
+        name: "LEDTR - Leitura de Dados em Tempo Real",
+        url: "https://sun.eduzz.com/653431"
+      },
+      {
+        name: "HortiFruti Delivery",
+        url: "https://hortifrutinm.vercel.app/"
       }
     ]
   },
@@ -63,12 +152,26 @@ const links = [
     name: 'LINKS QUE VÃO AJUDAR',
     content: [
       {
-        name: "DESAFIO TRYBE EBYTR",
-        url: "skjjs"
+        name: "Comece com Hardware - Arduino",
+        url: "https://www.arduino.cc/"
       },
       {
-        name: "DESAFIO TRYBE EBYTR",
-        url: "jhsjhs"
+        name: "Comece com Software - Anaconda(Python)",
+        url: "https://www.anaconda.com/products/individual"
+      },
+      {
+        name: "Comece com Hardware e Software - Code IoT",
+        url: "https://codeiot.org.br/"
+      }
+      ,
+      {
+        name: "Cursos de graça de Tecnologia - FIAP",
+        url: "https://on.fiap.com.br/local/movimentobrasildigital/"
+      }
+      ,
+      {
+        name: "Cursos na Udemy - Estude sempre",
+        url: "https://www.udemy.com/"
       }
     ]
   },
